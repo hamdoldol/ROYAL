2281ab178897412b7bd88f07646958c08b419054
hamdoldol <kgw150508@gmail.com>
2025-11-07 03:59:57 +0000

    mining.py 수정: 해시 찾기 로직의 조건문 정리 및 코드 가독성 향상

--- scr/mining.py
+++ scr/mining.py
@@ -12,14 +12,14 @@ def mining():
                                                     
         if plus == main:
             if hash == plus:
-                
-                else
-            print("mining hash! hash is", plus)
-            hashs.append(plus)
-            break  # 해시를 찾았으니 반복 종료
-            time.sleep(0.1)  # CPU 점유율 너무 높지 않게 잠깐 쉬기                                                                                                               
-            print("Mining complete! Hash list:", hash)
-            hashs =+ plus
+                pass
+            else:
+                print("mining hash! hash is", plus)
+                hashs.append(plus)
+                break  # 해시를 찾았으니 반복 종료
+                time.sleep(0.1)  # CPU 점유율 너무 높지 않게 잠깐 쉬기                                                                                                               
+                print("Mining complete! Hash list:", hash)
+                hashs =+ plus
              
 mining()
                                                                         
