15f3249984949592beef233fc5198435ae3bf9b6
hamdoldol <kgw150508@gmail.com>
2025-11-07 03:59:28 +0000

    main.py 및 mining.py 파일 추가: 기본적인 채굴 기능 구현

---
+++ scr/main.py
@@ -0,0 +1,8 @@
+import mining
+
+def main():
+    promport = input("1.mining 2.sell 3.buy")
+    if promport == 1 or mining :
+        mining.mining()
+
+main()
---
+++ scr/mining.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+def mining():
+    hashs = []
+        
+    while True:
+        ran1 = random.randrange(1, 1000000)
+        ran2 = random.randrange(1, 1000000)
+        plus = ran1 + ran2
+        main = random.randrange(1, 2000000)
+                                                    
+        if plus == main:
+            if hash == plus:
+                
+                else
+            print("mining hash! hash is", plus)
+            hashs.append(plus)
+            break  # 해시를 찾았으니 반복 종료
+            time.sleep(0.1)  # CPU 점유율 너무 높지 않게 잠깐 쉬기                                                                                                               
+            print("Mining complete! Hash list:", hash)
+            hashs =+ plus
+             
+mining()
+                                                                        
